de6ef7196756479837166de96cf0a3469b64a72f
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2020-09-16 17:33:15 +0300

    First commit

---
+++ src/RandomColor.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const green = '#39D1B4';
+
+export class RandomColor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: green };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor() {
+    const newColor =
+      '#' +
+      (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+    this.setState({ color: newColor });
+  }
+
+  render() {
+    return (
+      <div style={{ background: this.state.color }}>
+        <section>
+          <button onClick={this.changeColor}>Change color</button>
+          <h1>Change my color</h1>
+        </section>
+      </div>
+    );
+  }
+}
